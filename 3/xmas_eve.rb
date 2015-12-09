736ec8682937fb8f34b230462d5a07acb36517ba
Philip Wigg <phil@postdata.co.uk>
2015-12-09 13:35:28 +0000

    Day 3, part 2

--- 3/xmas_eve.rb
+++ 3/xmas_eve.rb
@@ -21,11 +21,20 @@ end
 i = IO.read('input.txt')
 
 location_list = []
-current_location = [0,0]
+santa_current_location = [0,0]
+robo_current_location = [0,0]
+robo_turn = false
 
 i.each_char do |c|
-  location_list << current_location
-  current_location = move_location(c, current_location)
+  if robo_turn
+    robo_current_location = move_location(c, robo_current_location)
+    location_list << robo_current_location
+    robo_turn = false
+  else
+    santa_current_location = move_location(c, santa_current_location)
+    location_list << santa_current_location
+    robo_turn = true
+  end
 end
 
 puts "#{location_list.uniq.count} houses got at least one present!"
