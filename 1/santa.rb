1fee13a488a19a0993883eb03a213353c804d92f
Philip Wigg <phil@postdata.co.uk>
2015-12-09 11:25:35 +0000

    Part 2 of day one.

--- 1/santa.rb
+++ 1/santa.rb
@@ -1,14 +1,32 @@
 i = IO.read('input.txt')
 
 floor = 0
+position = 0
+entered_basement = false
+basement_position = 0
 
 i.each_char { |c|
+
+  position += 1
+
   case c
     when "("
       floor += 1
     when ")"
       floor -= 1
   end
+
+  if floor == -1 && !entered_basement
+    basement_position = position
+    entered_basement = true
+  end
+    
 }
 
 puts "Santa is on floor #{floor}!"
+
+if entered_basement
+  puts "Santa first reach the basement at position #{basement_position}."
+else
+  puts "Poor Santa never reached the basement..."
+end
