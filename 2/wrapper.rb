e6ee3834104e5f285040e120396005c2b5de3c2c
Philip Wigg <phil@postdata.co.uk>
2015-12-09 12:34:30 +0000

    Part 2 of day 2

--- 2/wrapper.rb
+++ 2/wrapper.rb
@@ -4,13 +4,27 @@ def area(l,w,h)
   return  surface_area + smallest_side
 end
 
+def ribbon(l,w,h)
+  shortest = [ l+l+w+w,l+l+h+h,h+h+w+w ].min
+  cubic = l*w*h
+  return shortest + cubic
+end
+
 i = IO.read('input.txt')
 
-total = 0
+paper_total = 0
+ribbon_total = 0
 
 i.each_line do |line|
   dims = line.split('x')
-  total = total + (area(dims[0].to_i,dims[1].to_i,dims[2].to_i))
+  l = dims[0].to_i
+  w = dims[1].to_i
+  h = dims[2].to_i
+
+  paper_total = paper_total + area(l,w,h)
+  ribbon_total = ribbon_total + ribbon(l,w,h)
 end
 
-puts "Total is #{total}!"
+puts "Paper total is #{paper_total}"
+puts "Ribbon total is #{ribbon_total}"
+
